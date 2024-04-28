44dc7e235538ea423d620cc6743ea495b8af5761
layden diaz <layden@student.dev-island.org>
2024-04-27 16:43:35 -1000

    Set up project structure and configured Expo dependencies

--- DaKineWeatherMobileApp/src/App.js
+++ DaKineWeatherMobileApp/src/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
-
+import { registerRootComponent } from "expo";
 const Stack = createStackNavigator();
 
 function App() {
@@ -14,5 +14,5 @@ function App() {
     </NavigationContainer>
   );
 }
-
+registerRootComponent(App);
 export default App;
